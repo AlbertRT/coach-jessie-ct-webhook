eed12f6c1ced8657adaa1855a5a8faec8d31ac69
Albert <albertjokro22@gmail.com>
2025-02-23 11:45:53 +0700

    remove log

--- src/app/api/notion/pages/post/route.ts
+++ src/app/api/notion/pages/post/route.ts
@@ -3,7 +3,5 @@ import { NextRequest } from "next/server";
 export async function POST(req: NextRequest) {
 	const body = await req.json();
 
-	console.log(body);
-
 	return Response.json({ status: 200, body });
 }
