c3a2be7a095748f7c037e4828e9f92dac14c8113
Albert <albertjokro22@gmail.com>
2025-02-24 13:35:24 +0700

    new api response

--- src/app/api/notion/pages/patch/route.ts
+++ src/app/api/notion/pages/patch/route.ts
@@ -1,6 +1,6 @@
 import getPageId from "@/app/helper/getPageId";
 import { notionClient } from "@/app/lib/notion";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
 	const { nama, no_hp } = await req.json();
@@ -23,8 +23,13 @@ export async function POST(req: NextRequest) {
 			},
 		});
 
-		return Response.json({ response }, { status: 200 });
+		return NextResponse.json({
+			success: true,
+			msg: "Data updated",
+		});
 	} catch (error) {
+		console.log(error);
+
 		return Response.json({ error: `${error}` }, { status: 500 });
 	}
 }
